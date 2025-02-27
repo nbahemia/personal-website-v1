1ccbc252aed4d08f883780d851b0125687a2abf0
Naeem Bahemia <naeembahemia@gmail.com>
2025-02-27 10:13:33 -0500

    Attempted fix

--- src/components/Top_Tabs.tsx
+++ src/components/Top_Tabs.tsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion"
 
-import Resume from "../assets/NaeemBahemia_Resume.pdf"
 import Otter from "../assets/Otter_Icon.png"
+const Resume = "/resume.pdf";
 function Top_Tabs() {
 
     return (
@@ -9,18 +9,18 @@ function Top_Tabs() {
                 <img src={Otter} className="justify-start h-15 w-15 ml-4"/>
                 <ul className="flex flex-wrap -mb-px justify-end">
                     <li className="me-2">
-                        <motion.a href="#About" className="inline-block p-4 border-b-4 border-transparent rounded-t-1 hover:border-[#A0C878] transition duration-200 ease-in">
+                        <motion.a href="#About" className="inline-block p-4 border-b-4 border-transparent rounded-t-lg hover:border-[#A0C878] transition duration-200 ease-in">
                             About
                         </motion.a>
                     </li>
                     <li className="me-2">
-                        <motion.a href="#Projects" className="inline-block p-4 border-b-4 border-transparent rounded-t-1g hover:border-[#A0C878] transition duration-200 ease-in">
+                        <motion.a href="#Projects" className="inline-block p-4 border-b-4 border-transparent rounded-t-lg hover:border-[#A0C878] transition duration-200 ease-in">
                             Projects
                         </motion.a>
                     </li>
                     <li className="me-2">
-                        <motion.a href={Resume} target="_blank" className="inline-block p-4 border-b-4 border-transparent rounded-t-1g  hover:border-[#A0C878] transition duration-200 ease-in">
-                            Resume
+                        <motion.a href={Resume} target="_blank" rel="noopener noreferrer" className="inline-block p-4 border-b-4 border-transparent rounded-t-lg  hover:border-[#A0C878] transition duration-200 ease-in">
+                            View Resume
                         </motion.a>
                     </li>
                 </ul>
